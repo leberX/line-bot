70d4b4a5e481fb7beb91572dac51df72d3b8918d
jinna <jin.nakamura.m@gmail.com>
2025-12-15 06:20:16 +0900

    Fix port for render

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 
 const config = {
- channelAccessToken : process.env.CHANNEL_ACCESS_TOKEN
- channelSecret: process.env.CHANNEL_SECRET
+ channelAccessToken : process.env.CHANNEL_ACCESS_TOKEN,
+ channelSecret : process.env.CHANNEL_SECRET,
 };
 
 const app = express();
