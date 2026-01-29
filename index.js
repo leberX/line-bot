e8bc68848d6d1a1a4397391c549ef9802a26ea7e
jinna <jin.nakamura.m@gmail.com>
2026-01-29 09:51:26 +0900

    remove defalt thank you Message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function handleEvent(event) {
   return await client.replyMessage(event.replyToken, [
     {
     type: "text",
-    text: "テスト返信"
+    text: "初めまして！　あなたのお世話をするロボットです！"
   }
   ]);
 }//
