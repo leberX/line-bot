4e730475aaa6ff1bcb8ce61b61279b4c5dbe4563
jinna <jin.nakamura.m@gmail.com>
2026-06-27 11:36:51 +0900

    edit

--- index.js
+++ index.js
@@ -382,7 +382,7 @@ const { data: parents, error } = await supabase
     }});
   
     // ===== 未返信検知（3時間ごと）=====
-    cron.schedule('* * * * *', async () => {
+    cron.schedule('0 */3 * * *', async () => {
   console.log("⏳ 未返信チェック");
 
   const LIMIT = 24 * 60 * 60 * 1000; // 24時間
