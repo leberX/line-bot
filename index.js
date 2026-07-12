c8b8070c046248b2278d75879718a571d3b64de6
jinna <jin.nakamura.m@gmail.com>
2026-07-12 20:36:34 +0900

    edit

--- index.js
+++ index.js
@@ -431,7 +431,7 @@ cron.schedule('* * * * *', async () => {
       const { error: updateError } = await supabase
         .from("users")
         .update({
-          notified: false
+          notified: true
         })
         .eq("user_id", parent.user_id);
 
